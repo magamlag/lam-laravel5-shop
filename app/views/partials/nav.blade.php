0ef207d75992af35ae3261cce2c47bd759557dc1
Andrey <lagovskiy@gmail.com>
2014-11-28 21:59:34 +0200

    Removed few unnessary menu items

--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -71,8 +71,6 @@
 											 <ul role="menu" class="sub-menu">
 													<li><a href="shop.html">Products</a></li>
 													<li><a href="product-details.html">Product Details</a></li>
-													<li><a href="checkout.html">Checkout</a></li>
-													<li><a href="cart.html">Cart</a></li>
 													<li>{{HTML::link('users/signin', 'Login')}}</li>
 											 </ul>
                       </li>
