5e3e734b545d49165d69a76fe4796daef8f3711a
Andrey <lagovskiy@gmail.com>
2014-12-03 12:13:20 +0200

    Changed settings for SMTP

--- app/config/mail.php
+++ app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => 'smtp.gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'lagovskiy@gmail.com', 'name' => 'Awesome Laravel 4 Auth App'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => 'lagovskiy@gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => null,
+	'password' => 'visualadonet',
 
 	/*
 	|--------------------------------------------------------------------------
