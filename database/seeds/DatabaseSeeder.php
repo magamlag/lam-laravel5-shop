94b2be05ce52356043924bb767faf84c97106c4d
magamlag <lagovskiy@gmail.com>
2015-05-06 16:00:46 -0400

    Updated Seeder files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,10 +1,9 @@
 <?php
-
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder {
-
+	protected $faker;
 	/**
 	 * Run the database seeds.
 	 *
@@ -13,8 +12,21 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Model::unguard();
+		//$this->call("ProductTableSeeder");
+		//$this->call("OrderTableSeeder");
+		$this->call("OrderItemTableSeeder");
+	}
 
-		// $this->call('UserTableSeeder');
+	public function getFaker()
+	{
+		if (empty($this->faker))
+		{
+			$faker = Faker\Factory::create();
+			$faker->addProvider(new Faker\Provider\Base($faker));
+			$faker->addProvider(new Faker\Provider\Lorem($faker));
+		}
+
+		return $this->faker = $faker;
 	}
 
 }
